064573ca39cccc0d1562e19b63270186e9dc917f
catalinajitarasu <larisajitarasu@gmail.com>
2022-12-05 13:30:28 +0200

    functie recursiva care creaza files and directories

--- main.py
+++ main.py
@@ -8,28 +8,30 @@ import os
 # https://www.stechies.com/encoding-decoding-base64-strings-python/
 # https://stackabuse.com/encoding-and-decoding-base64-strings-in-python/
 
+# https://datagy.io/python-nested-dictionary/
+
+
+def iterate_dict(dict_to_iterate, path):
+    for key, value in dict_to_iterate.items():
+        print(path)
+        if type(value) == dict:
+            print(key)
+            path = os.path.join(path, str(key))
+            os.makedirs(path)
+            iterate_dict(value, path)
+            path = os.path.dirname(path)
+        else:
+            base64_bytes = value.encode('utf-8')
+            path = os.path.join(path, str(key))
+            with open(path, "wb") as file_to_save:
+                decoded_data = base64.decodebytes(base64_bytes + b'==')
+                file_to_save.write(decoded_data)
+            path = os.path.dirname(path)
+            print(key + ":" + value)
+
+
 if __name__ == '__main__':
     path_dir = "C:\\Users\\Catalina\\OneDrive\\Desktop\\Proiect-Python\\root"
     with open('C:\\Users\\Catalina\\OneDrive\\Desktop\\Proiect-Python\\Laborator\\file.json') as json_file:
         data = json.load(json_file)
-    print(len(data))
-    for i in data.items():
-        # print(i)
-        # cand valoarile cheilor sunt stringuri se creeaza in root fisierele
-        if type(i[1]) is str:
-            base64_bytes = i[1].encode('utf-8')
-            # print(base64_bytes)
-            path = os.path.join(path_dir, str(i[0]))
-            with open(path, "wb") as file_to_save:
-                decoded_data = base64.decodebytes(base64_bytes + b'==')
-                # print(decoded_data)
-                file_to_save.write(decoded_data)
-        # cand valorile cheilor sunt dictionare se creaza directoarele in root dar
-        # ce este dictionar trebuie creat in fiecare subdirector
-        if type(i[1]) is dict:
-            # print(str(i[0]))
-            # creez directorul (cheia)
-            path = os.path.join(path_dir, str(i[0]))
-            os.makedirs(path)
-            # in interiorul directorului trebuie sa formez fisierele/directoarele din dictionar
-        
+    iterate_dict(data, path_dir)
