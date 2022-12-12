53a351befede6ebc02736cdf714b2900f5abc10c
catalinajitarasu <larisajitarasu@gmail.com>
2022-12-12 17:37:05 +0200

    cazurilor de eroare si lucru cu sys.argv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import os
+import sys
 
 
 # unde am transformat in baza 64
@@ -12,25 +13,27 @@ import os
 # https://datagy.io/python-nested-dictionary/
 
 
-def generate_structure(dictionary, path,tab):
+def generate_structure(dictionary, path, tab):
     for key, value in dictionary.items():
         # print(path)
         if type(value) == dict:
             # print(key)
             path = os.path.join(path, str(key))
-            tab = tab + '---'
-            print(tab, key)
-            os.makedirs(path)
-            generate_structure(value, path, tab)
-            path = os.path.dirname(path)
-            tab = tab[:-3]
+            if os.path.exists(path):
+                raise Exception('[ERROR] - Directory already exists!')
+            else:
+                tab = tab + '---'
+                print(tab, key)
+                os.makedirs(path)
+                generate_structure(value, path, tab)
+                path = os.path.dirname(path)
+                tab = tab[:-3]
         else:
             base64_bytes = value.encode('utf-8')
             path = os.path.join(path, str(key))
             tab = tab + '---'
 
             with open(path, "wb") as file_to_save:
-                # decoded_data = base64.decodebytes(base64_bytes + b'==')
                 decoded_data = base64.decodebytes(base64_bytes)
                 file_to_save.write(decoded_data)
                 print(tab, key, ":", decoded_data)
@@ -40,9 +43,35 @@ def generate_structure(dictionary, path,tab):
 
 
 if __name__ == '__main__':
-    path_dir = "C:\\Users\\Catalina\\OneDrive\\Desktop\\Proiect-Python\\root"
-    with open('C:\\Users\\Catalina\\OneDrive\\Desktop\\Proiect-Python\\Laborator\\test1.json') as json_file:
-        data = json.load(json_file)
+    if len(sys.argv) < 3:  # create_structure.py root_folder_path structure_json_file_path
+        raise Exception('[ERROR] - Invalid number of arguments!')
+
+    path_dir = sys.argv[1]
+    json_file_path = sys.argv[2]
+
+    if os.path.exists(path_dir) is False:
+        raise Exception('[ERROR] - Directory not found!')
+
+    if os.path.isdir(path_dir) is False:
+        raise Exception('[ERROR] - Path is not directory')
+
+    if os.path.exists(json_file_path) is False:
+        raise Exception('[ERROR] - File not found!')
+
+    if os.path.isfile(json_file_path) is False:
+        raise Exception('[ERROR] - Path is not file!')
+
+    if os.path.getsize(json_file_path) == 0:
+        raise Exception('[ERROR] - Json file is empty!')
+
+    try:
+        with open(json_file_path) as json_file:
+            data = json.load(json_file)
+    except Exception as e:
+        print(e)
+    if data:
         tab = ""
-    print("root")
-    generate_structure(data, path_dir, tab)
+        print("root")
+        generate_structure(data, path_dir, tab)
+    else:
+        raise Exception('[ERROR] - Dictionary is empty!')
